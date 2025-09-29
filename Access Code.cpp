77422ede20db0e1904bb45a05c03fffd3a95b42c
65NinadA3 <ninadphatak123@gmail.com>
2025-09-29 20:59:54 +0530

    Create Access Code.cpp

---
+++ Access Code.cpp
@@ -0,0 +1,22 @@
+//Name : Ninad Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main(){
+    double code;
+    cout << "Enter the private code:";
+    cin >> code;
+    try{
+        if(code!=9762){
+            throw code;
+        }
+        else{
+            cout << "Access Granted" << endl;
+        }
+    }
+    catch(double wrongCode){
+        cout << "\nERROR: Entered wrong code"<<endl;
+    }
+    return 0;
+}
